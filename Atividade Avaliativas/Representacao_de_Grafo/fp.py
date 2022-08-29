d6d6c5f2a8eb874f30cc25c1b8e67971f07d86d6
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-08-29 13:30:00 -0300

    linha limpa

--- Atividade Avaliativas/Representacao_de_Grafo/fp.py
+++ Atividade Avaliativas/Representacao_de_Grafo/fp.py
@@ -5,14 +5,31 @@ import os.path
 #setando o path
 path = './database/'
 
+
+#func
+def criarLista(rString):
+  print(rString)
+
+
+
 #Pegando Argumento do Command Line
 fileName = sys.argv[1]
 dirname = os.path.dirname(path)
-pathConcat = dirname + '/' + fileName + '.txt'
+verticeInput = sys.argv[2]
+
+#Testa se Tem .txt no campo do nome do arquivo
+if ".txt" in fileName:
+  pathConcat = dirname + '/' + fileName
+else:
+  pathConcat = dirname + '/' + fileName + ".txt"
 
 with open(pathConcat) as file:
   linha = file.readlines()
-  
+
 
 #Limpa a linha e deixa pronto para utilizar os valores
-print(linha[0].strip())
+for i in range(1, len(linha)):
+  removeSpace = linha[i].strip()
+  splits = removeSpace.split()
+  criarLista(splits)
+
