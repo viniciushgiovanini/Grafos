1261a4342cb7db1a337ea4a1e2ec98d74f169a21
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-10-12 00:34:32 -0300

    criando menu switch case

--- Atividade Avaliativas/TP01/py/core.py
+++ Atividade Avaliativas/TP01/py/core.py
@@ -1,10 +1,36 @@
 #imports
+from re import match
 from gerarGrafos import criarGrafos
 import time
 start = time.perf_counter()
 
-gEule = criarGrafos()
-class1 = gEule.criarEulerianos(100)
+
+print("0 - Encerrar o Programa | 1 - Geração dos Grafos")
+numeroCase = int(input("Escreva o número desejado !\n"))
+
+vLoop = True
+while(vLoop):
+  if numeroCase == 0:
+    vLoop = False
+    print("Encerrando o Programa")
+    break
+  elif numeroCase == 1:
+    print("a - Gerar grafo euleriano | b - gerar grafo semi euleriano | c - gerar grafo não euleriano")
+    numeroCase2 = input("Insira a letra desejada: ")
+    if numeroCase2 == "a":
+      tamGrafo = int(input("Insira o tamanho do grafo euleriano desejado (100, 1000 ou 100.000): "))
+      gEule = criarGrafos()
+      class1 = gEule.criarEulerianos(tamGrafo)
+    elif numeroCase2 == "b":
+      pass
+    elif numeroCase2 == "c":
+      pass
+    else:
+      print("Valor inserido não existe !")    
+  else:
+    print("Valor inserido não existe !")
+  print("0 - Encerrar o Programa | 1 - Geração dos Grafos")  
+  numeroCase = int(input("Escreva o número desejado !\n"))
+
 end = time.perf_counter()
 print(end - start)
-print(class1)
