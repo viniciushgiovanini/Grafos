d02200ddbb51babb0c5f5c0660e250c7403d6822
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-10-12 19:18:21 -0300

    Geracao Grafos Eulerianos 100% #1

--- Atividade Avaliativas/TP01/py/gerarGrafos.py
+++ Atividade Avaliativas/TP01/py/gerarGrafos.py
@@ -4,33 +4,38 @@ import os
 
 class criarGrafos:
 
-  #funcoes globais  
-  #------------------------------------------
-  #funcoes do grafo Euleriano  
-  
-  def verificarARQGrafofEuleriano(self):
+  #funcoes globais 
+  def isPath(self, caminho):
     resp = False
-    caminho = 'data/grafosEulerianos.txt'
     if (os.path.exists(caminho)):
       resp = True
-    return resp
-  
-  
-  def gerarVerticesEulerianos(self, cont, qtd, obj, listaRR):
+    return resp 
+  #------------------------------------------
+  #funcoes do grafo Euleriano  
+  def gerarVerticesEulerianos(self, cont, grau, obj, listaRR):
     cont2 = 0
-    
-    while(cont2 < qtd):
+    contadorLaco = 1
+    marcadorLaco = False
+    valor1 = -1
+    while(cont2 < grau):
       # Salvar cont no arquivo e numero sorteado
-      if len(listaRR) != 0: 
+      if len(listaRR) != 0 and marcadorLaco == False: 
        destinoAresta = listaRR[0]
-       listaRR.remove(destinoAresta)
+       
       else:
-       destinoAresta = 1
+       destinoAresta = listaRR[contadorLaco]
+       contadorLaco = contadorLaco+1
     
-      marcadorLaco = False
-      if destinoAresta == cont:
+      
+      if destinoAresta == cont or destinoAresta == valor1:
         marcadorLaco = True
-        qtd = qtd + 1
+        grau = grau + 1
+      else:
+        valor1 = destinoAresta
+        contadorLaco = 1
+        marcadorLaco = False
+        listaRR.remove(destinoAresta)
+        
         
        
        
@@ -43,18 +48,12 @@ class criarGrafos:
       cont2 = cont2 + 1
     # listaRR = []  
       
-    
-      
-
-
-
   def criarEulerianos(self,tamReq, nomeArq):
     
     # Verificar se o arquivo existe
-    arquivoExiste = self.verificarARQGrafofEuleriano()
-    
-    
-    strInterpolacao = 'data/grafosEulerianos/' + nomeArq + '.txt'    
+    strInterpolacao = 'data/grafosEulerianos/' + nomeArq + '.txt'
+    arquivoExiste = self.isPath(strInterpolacao)   
+        
     
     if (arquivoExiste == False):
       oPP = open(strInterpolacao, 'wb+')
@@ -67,38 +66,108 @@ class criarGrafos:
     
     
     
-    cont = 1
-    #  qtd = random.randrange(1, 11, 2)
-    qtd = 2
+    
+    #  grau = random.randrange(1, 11, 2)
+    grau = 4
     if tamReq == 1000:
-     qtd = 4
-    else:
-     if tamReq == 1000000:
-      qtd = 2
+     grau = 4
+    elif tamReq == 1000000:
+      grau = 2
     
     # referente as duas linhas salvas la em cima
-    qtdLinhas = 2 + (qtd * int(tamReq))
+    cont2Chega = tamReq/10
+    qtdLinhas = 1 + ((grau * int(tamReq)) - ((grau-2) * int(cont2Chega)))
     obj.write(str(tamReq) + "_" + str(qtdLinhas) + "\n")
         
     listaRR = []
-    listaRang = range(1, tamReq+1)
+    listaRang= range(1, tamReq+1)
+    for item in listaRang:
+     listaRR.append(item)
+  
+    concatCont = 0
+    while (concatCont < grau-1):
+      listaRR = listaRR + listaRR
+      concatCont = concatCont + 1
+      
+    random.shuffle(listaRR)
+    
+    
+    
+    grau2 = grau-2
+    cont = 1
+    cont2 = 1
+    isGrau2 = False
+    temp = -1
+    while cont <= tamReq:
+      
+      if cont2 == cont2Chega and cont2Chega != 0:
+        temp = grau
+        grau = grau2
+        cont2 = 0
+        isGrau2 = True
+      
+      self.gerarVerticesEulerianos(cont, grau, obj, listaRR)     
+      cont2 = cont2 + 1
+      cont = cont + 1
+      
+      if isGrau2:
+        grau = temp
+        isGrau2 = False
+    
+    
+    obj.close()  
+
+  #------------------------------
+  #  funcoes do grafo semi euleriano
+  
+  def gerarVerticesSemiEulerianos(cont, grauPar, grauImpar, obj, listaRR):
+    cont2 = 0
+    while (cont2 <= cont):
+      cont2 = cont2 + 1
+  
+  def criarSemiEulerianos(self, qtdVertice, nomeArq):
+    
+    strCaminho = 'data/grafosSemiEulerianos/' + nomeArq + '.txt'
+    arquivoExiste = self.isPath(strCaminho)
+    
+    if (arquivoExiste == False):
+      oPP = open(strCaminho, 'wb+')
+      oPP.close()
+    else:
+      open(strCaminho, 'w').close()
+            
+    
+    obj = open(strCaminho, 'r+')
+    
+    grau = 2
+    grau2 = 1
+    if qtdVertice == 1000:
+     grau = 4
+     grau2 = 3
+    else:
+     if qtdVertice == 1000000:
+      grau = 6
+      grau2 = 5
+    
+    # referente as duas linhas salvas la em cima
+    qtdLinhas = 2 + ((grau + grau2) * int(qtdVertice))
+    obj.write(str(qtdVertice) + "_" + str(qtdLinhas) + "\n")
+    
+    
+    listaRR = []
+    listaRang = range(1, qtdVertice+1)
     for item in listaRang:
      listaRR.append(item)
      listaRR.insert(0,item)
     
     random.shuffle(listaRR)
-    
-    
-    
-    while cont <= tamReq:
-     self.gerarVerticesEulerianos(cont, qtd, obj, listaRR)     
+    cont = 1
+    while cont <= qtdVertice:
+     self.gerarVerticesSemiEulerianos(cont, grau, grau2, obj, listaRR)     
      cont = cont + 1
+    obj.close()
     
     
-    obj.close()  
-  
-  # def criarSemiEulerianos():
-  #   pass
   # def criarNaoEulerianos():
   #   pass   
   
