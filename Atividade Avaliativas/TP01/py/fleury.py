068ce4c067bd5bf41e51ba1abfde9c1d51f1e46b
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-10-19 16:14:24 -0300

    #4 Fleury não euleriano 100%

--- Atividade Avaliativas/TP01/py/fleury.py
+++ Atividade Avaliativas/TP01/py/fleury.py
@@ -1,11 +1,48 @@
-from contextlib import nullcontext
-
-
 class fleuryAlg:
+  
+  def tratarLinha(self, item):
+    numerosTratados = []
+    hasNumber1 = False
+    cont = 0
+    concatNumber1 = ""
+    concatNumber2 = ""
+    while cont < len(item):
+      if item[cont] != "\n":
+        if item[cont] == " ":
+          hasNumber1 = True
+        elif hasNumber1 == False:
+          concatNumber1 = concatNumber1 + item[cont]   
+        elif hasNumber1 == True and item[cont] != " ":
+          concatNumber2 = concatNumber2 + item[cont]
+      cont = cont + 1
+    numerosTratados.append(int(concatNumber1))
+    numerosTratados.append(int(concatNumber2))
+    
+    return numerosTratados
+    
+    
+  def fleurynotIsEuleriano(self, listaNumber, listaGuardarVértice):
+      
+   if len(listaGuardarVértice) == 0:
+    listaGuardarVértice.append(listaNumber[0])
+   elif listaGuardarVértice[0] == listaNumber[0]:
+    listaGuardarVértice.append(listaNumber[0])
+   elif listaGuardarVértice[0] != listaNumber[0]:
+    qtdV = len(listaGuardarVértice)
+    listaGuardarVértice.clear()
+    listaGuardarVértice.append(listaNumber[0])
+    if qtdV % 2 != 0:
+      return True
+    
+    
+    
+      
+
+  
   def fleuryInicial(self, nomeArq, entradaFL):
     
     strInterpolacao = ''
-    entradaFL = entradaFL.lower()
+    entradaFL = int(entradaFL)
     if entradaFL == 1:
        strInterpolacao = 'data/grafosEulerianos/' + nomeArq + '.txt'
     elif entradaFL == 2:
@@ -17,7 +54,27 @@ class fleuryAlg:
       return
     
     reader = open(strInterpolacao, "r")  
+    arquivinho = reader.readlines()
+    contadorpularPrimeiraLinha = 0
     
-    while reader!=nullcontext:
-      print(reader.read)
+    # Variaveis Grafos Não eulerianos    
+    listaGuardarVértice = [] #esse lista é apra guardar os vértices para testar se é euleriano
     
+    for item in arquivinho:
+      if contadorpularPrimeiraLinha != 0:
+        linhaTratada = self.tratarLinha(item)
+        isNotEulerian =  self.fleurynotIsEuleriano(linhaTratada, listaGuardarVértice)
+        
+        if isNotEulerian:
+          print("\nEsse grafo NÃO é euleriano\n")
+          return
+        
+      else:
+        contadorpularPrimeiraLinha = contadorpularPrimeiraLinha + 1
+     
+ 
+    
+     
+  
+  
+  
