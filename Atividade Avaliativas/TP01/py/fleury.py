170d6778bef9d17f0f4ba23d7f890bfb26bbbfb1
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-10-20 08:48:09 -0300

    #4 avancando fleury

--- Atividade Avaliativas/TP01/py/fleury.py
+++ Atividade Avaliativas/TP01/py/fleury.py
@@ -20,19 +20,26 @@ class fleuryAlg:
     
     return numerosTratados
     
-    
-  def fleurynotIsEuleriano(self, listaNumber, listaGuardarVértice, listaSUPREME, listaListaNumber):
-    
-    
-   
-    
+  def tratarPrmeiraLinhaQTD(self, item):
+    cont = 0
+    marcador = False
+    qtdNumero = ""
+    while cont < len(item):
+      if item[cont] == "_" or marcador:
+        if marcador and item[cont] != "\n":
+         qtdNumero = qtdNumero + item[cont]
+        marcador = True
+      cont = cont + 1
+    return int(qtdNumero)
+  
+  def realizandoFleury(self, listaNumber, listaGuardarVértice, listaSUPREME, listaListaNumber, ultimoElemento):
    if len(listaGuardarVértice) == 0:
     listaGuardarVértice.append(listaNumber[0])
     listaListaNumber.append(list(listaNumber).copy())
-   elif listaGuardarVértice[0] == listaNumber[0]:
+   elif (listaGuardarVértice[0] == listaNumber[0]) and not ultimoElemento:
     listaGuardarVértice.append(listaNumber[0])
     listaListaNumber.append(list(listaNumber).copy())
-   elif listaGuardarVértice[0] != listaNumber[0]:
+   elif (listaGuardarVértice[0] != listaNumber[0]) and not ultimoElemento:
     listaSUPREME.append(listaListaNumber.copy())
     listaListaNumber.clear()   
     qtdV = len(listaGuardarVértice)
@@ -41,20 +48,36 @@ class fleuryAlg:
     listaListaNumber.append(list(listaNumber).copy())
     if qtdV % 2 != 0:
       return True
+   elif ultimoElemento:
+     listaGuardarVértice.append(listaNumber[0])
+     listaListaNumber.append(list(listaNumber).copy())
+     listaSUPREME.append(listaListaNumber.copy())
+     listaListaNumber.clear()  
+   
   #  print(listaSUPREME)
-    
   
-  def fleuryPrinci(self, listaSUPREME):
-    tamList = len(listaSUPREME)
+  def selecionandoMenorElemento(self, lista):
+    resp = 10000000000000
+    listaResposta = []
+    for item in lista:
+     if item[1] < resp:
+      resp = item[1]
+      listaResposta = item.copy()
+    return listaResposta
+  
+  def analisandoFleury(self, listaSUPREME, qtdLinhas):
     cont = 0
     destino = 0
     caminhoLista = []
-    while cont < tamList:
+    while cont < qtdLinhas-1:
       inicio = listaSUPREME[destino]
-      caminhoLista.append(list(inicio[0]).copy())
-      destino = inicio[1]            
+      posicaoLista = listaSUPREME.index(inicio)
+      menorValorEntreOsVertices = list(self.selecionandoMenorElemento(inicio))
+      caminhoLista.append(menorValorEntreOsVertices.copy())
+      listaSUPREME[posicaoLista].remove(menorValorEntreOsVertices)    
+      destino = menorValorEntreOsVertices[1] - 1            
       cont = cont + 1
-    
+    return caminhoLista
       
 
   
@@ -79,24 +102,35 @@ class fleuryAlg:
     # Variaveis Grafos Não eulerianos    
     listaGuardarVértice = [] #esse lista é apra guardar os vértices para testar se é euleriano
     listaListaNumber = []
+    #qtd_Linha
+    qtddeLinhas = 1
     # Fim variaveis grafos não eulerianos
     listaVerticeeArestas = []
+    contadorLoopPrinci = 0
+    flagcontadorLoopPrinciUltimoElement = False
     for item in arquivinho:
       
       if contadorpularPrimeiraLinha != 0:
         linhaTratada = self.tratarLinha(item)
                
-        
+        # Estou fazendo o flag com a quantidade de linha, melhor fazer com o ultimo elemento.
         
         # Testar se o grafo é Euleriano  
-        isNotEulerian =  self.fleurynotIsEuleriano(linhaTratada, listaGuardarVértice, listaVerticeeArestas, listaListaNumber)
+        isNotEulerian =  self.realizandoFleury(linhaTratada, listaGuardarVértice, listaVerticeeArestas, listaListaNumber, flagcontadorLoopPrinciUltimoElement)
+        if contadorLoopPrinci == qtddeLinhas -3:
+          flagcontadorLoopPrinciUltimoElement = True
+        contadorLoopPrinci = contadorLoopPrinci + 1
         if isNotEulerian:
           print("\nEsse grafo NÃO é euleriano\n")
           return
       else:
+        qtddeLinhas = self.tratarPrmeiraLinhaQTD(item)
         contadorpularPrimeiraLinha = contadorpularPrimeiraLinha + 1
-    self.fleuryPrinci(listaVerticeeArestas)   
- 
+    caimhoOuTrajeto = []    
+    caimhoOuTrajeto = list(self.analisandoFleury(listaVerticeeArestas, qtddeLinhas))
+    for item in caimhoOuTrajeto:
+      print( str(item) + "\n")
+     
     
      
   
