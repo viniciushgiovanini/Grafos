4c8c9b37ccecc4ac307e93cacff9d57966ab6f27
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-10-25 22:18:49 -0300

    #2 busca em profundidade 1/2 naive

--- Atividade Avaliativas/TP01/py/naive.py
+++ Atividade Avaliativas/TP01/py/naive.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 class naivePonte:
   
   def inverterElemento(self, element):
@@ -8,31 +11,41 @@ class naivePonte:
     valorNew[1] = temp0
     return valorNew
   
-  def descobrirPonteFlury(self, valor, listaSup):
-    # Ta quase funcionando, só que para descobrir que é ponte eu tenho que navegar ate 
-    # o caminho verificar se tem o caminho de volta e verificar se o tamanho da lista é 0
-    # Até agora eu consigo ir na lista e verificar se é 0 mas não consigo se tiver um caminho maior.
-    
-    
-    isPonte = False
-    valorInvert = self.inverterElemento(valor)
-    destino = valor[1]
-    tamLista = (len(listaSup[destino-1]))
-    if  (tamLista == 0) or (tamLista)==1:
-      isPonte = True
-    elif tamLista> 1:
-      temInvert = False
-      tamReal = (len(listaSup[destino-1]))
-      for item in listaSup[destino-1]:
-        if item == valorInvert:
-          temInvert = True
-      if temInvert:
-        tamReal = tamReal -1
+  def buscaProf(self, valor,l,  listaSup):
+   loop = True
+   lLimpo = l
+   b = deepcopy(listaSup)
+   while loop:
+    newValor = self.selecionandoMenorElemento(lLimpo) 
+    l = b[newValor[1]-1] 
+    if len(l)>1:
+      lLimpo = self.testarCiclodeVoltaRemovenaListComum(l, newValor)
+      if newValor[1] == valor[0]:
+        return False
+      else:
+        b[newValor[0]-1].remove(newValor)
+    else:
+      return True    
       
-      if tamReal == 0:
-        isPonte = True
-        
-         
+  def descobrirPonteFlury(self, valor, listaSup):
+    # Esse método é responsável por selecionar as arestas do vertice escolhido e mandar para busca em largura tentar
+    # chegar nele mesmo, caso caonsiga não precisa testar com os outros
+    isPonte = False
+    deslocalmentoInicial = listaSup[valor[1]-1]    
+    l = self.testarCiclodeVoltaRemovenaListComum(deslocalmentoInicial, valor)
+    tamL = len(l)
+    cont = 0
+    loop = True
+    while loop:
+      resp = self.buscaProf(valor, l, listaSup)
+      if resp == False:
+        return False
+      elif cont == tamL: 
+        return True
+      else:
+        l.remove(self.selecionandoMenorElemento(deslocalmentoInicial))
+        cont = cont + 1
+    
     return isPonte
   
   def selecionandoMenorElemento(self, lista):
@@ -54,21 +67,21 @@ class naivePonte:
     
     for item in valorDestino:
      if item == valor2:
-      listaSup[destino-1].remove(valor2)      
-  
-  def testarCiclodeVoltaResp(self, listaSup, valor):
+      listaSup[destino-1].remove(valor2)  
+      
+  def testarCiclodeVoltaRemovenaListComum(self, lista, valor):
     valor2 = valor.copy()
     destino = valor[1]
     tmp1 = valor[0]
-    valorDestino = listaSup[destino-1]
+    valorDestino = lista
     valor2[0] = destino
     valor2[1] = tmp1
-    resp = False
+    
     for item in valorDestino:
      if item == valor2:
-      resp = True
-    return resp
- 
+      lista.remove(valor2)     
+    return lista
+  
   def selecionarProximoCaminho(self, inicio, listaSup):
     inicio2 = inicio.copy()
     isPonte = False
@@ -82,6 +95,3 @@ class naivePonte:
      else:
        inicio2.pop(0)
     return menorValor 
-    
-    
-   
