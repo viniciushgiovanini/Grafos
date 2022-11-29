2c043912f724c669ac7e619804730af36a13d042
Vinícius Henrique Giovanini <vgiovanini@sga.pucminas.br>
2022-11-28 22:08:52 -0300

    Comentarios e próximas etapas

--- Atividade Avaliativas/TP02/Program.cs
+++ Atividade Avaliativas/TP02/Program.cs
@@ -1,56 +1,14 @@
 ﻿class Program
 {
-  private static bool selectionSort(int[] ver, int busca)
-  {
-    bool verticeExistente = false;
-
-    for (int i = 0; i < ver.Length; i++)
-    {
-      if (ver[i] == busca)
-      {
-        verticeExistente = true;
-      }
-    }
-    return verticeExistente;
-  }
-  private static int qtdElementosArray(int[] a)
-  {
-    int contador = 0;
-
-    foreach (var i in a)
-    {
-      if (i != 0)
-      {
-        contador++;
-      }
-    }
-    return contador;
-  }
   private static int contarTam(String l)
   {
     int tam = 0;
     String[] vet = l.Split(' ');
     tam = Int32.Parse(vet[0]);
 
-
-
     return tam;
   }
 
-  private static int contarTamArestas()
-  {
-    using var file = new StreamReader("./graph.txt");
-    String? LinhaTam;
-    int contador = 0;
-    while ((LinhaTam = file.ReadLine()) != "fim")
-    {
-      if (LinhaTam != null && LinhaTam != "n  m" && LinhaTam != "Inci")
-      {
-        contador++;
-      }
-    }
-    return contador;
-  }
   public static void leituraGrafo(String arqName, matrizadj ma)
   {
     bool criouMatrizAdj = false;
@@ -60,10 +18,11 @@
     String? linha;
     using var file = new StreamReader(arqName);
 
+    // Faz a leitura da primeira linha contendo a qtd de vértice e aresta.
     while ((linha = file.ReadLine()) != null)
     {
 
-
+      // Se não existir matriz ele cria uma matriz com tamamanho da qtd de vértices.
       if (!criouMatrizAdj)
       {
         tamanhoMatriz = contarTam(linha);
@@ -71,17 +30,20 @@
         criouMatrizAdj = true;
       }
 
+      // faz a leitura linha por linha até o final do arquivo.
       String? linha2;
       while ((linha2 = file.ReadLine()) != null)
       {
         if (linha2 != null)
         {
+          // Criação da Matriz de Adjacencia 
+          // Pega a linha e coloca o número 1 na posicao da matriz
           ma.receberDados(linha2, tamanhoMatriz);
         }
 
       }
 
-      ma.imprimirMatrizAdj(tamanhoMatriz);
+      // ma.imprimirMatrizAdj(tamanhoMatriz);
     }
 
   }
---
+++ Atividade Avaliativas/TP02/README.md
@@ -0,0 +1,21 @@
+# Segundo Trabalho Prático de Grafos  
+
+## Informações
+
+- Basicamente esse trabalho consiste em pegar um grafo, inserir o vértice de origem e destino e ver quantos caminhos existem entre eles.  
+- Os grafos foram pegos do primeiro trabalho não avaliativo.  
+- O **Enunciado** do trabalho pode ser encontrado [AQUI](doc/tgc-cc-tp02-2022-02.pdf).  
+
+## Etapas
+
+[X] É preciso fazer o menu finalizar.  
+[X] Precisa fazer ele identificar erro na digitacao do arquivo de entrada.  
+[X] Fazer método de busca.  
+
+## Linguagens de Programação
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" width="50px"/>
+
+## Desenvolvimento ✏
+
+**Feito por**: [Vinícius Henrique Giovanini](https://github.com/viniciushgiovanini)
